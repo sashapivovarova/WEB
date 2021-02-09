a39a5c76787516334cbba564730f3fce0b83968a
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2021-02-09 23:46:12 +0900

    Separating JS file

---
+++ colors.js
@@ -0,0 +1,33 @@
+var Body = {
+  setColor:function(color) {
+    document.querySelector('body').style.color = color;
+  },
+  setBackgroundColor:function(color) {
+    document.querySelector('body').style.backgroundColor = color;
+  }
+}
+var Links = {
+  setColor:function(color){
+    var alist = document.querySelectorAll('a');
+    var i = 0;
+    while(i < alist.length) {
+      console.log(alist[i]);
+      alist[i].style.color = color;
+      i = i + 1;
+    }
+  }
+}
+function nightDay(self){
+  var target = document.querySelector('body');
+  if(self.value==='night'){
+    Body.setColor('white');
+    Body.setBackgroundColor('black');
+    self.value='day';
+    Links.setColor('powderblue');
+  } else {
+    Body.setColor('black');
+    Body.setBackgroundColor('white');
+    self.value='night';
+    Links.setColor('blue');
+  }
+}
